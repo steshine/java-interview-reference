9360d33e2401ac35e51d26b51d4bc7f0069ac5a5
skychen <cp123002@gmail.com>
2018-08-09 23:57:29 +0800

    netty practice . just discard server

---
+++ netty-practice/src/main/java/cn/steshine/netty/discard/DiscardServerHandler.java
@@ -0,0 +1,21 @@
+package cn.steshine.netty.discard;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
+        System.out.print(msg);
+        // Discard the received data silently.
+        ((ByteBuf) msg).release(); // (3)
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
